a9dd25e027a80b728b5c9230e99bc2fb3c6758bb
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-07-13 16:32:37 +0400

    UDP server start point

---
+++ Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "Udp Server";
+
+            var server = new UdpServer(5005);
+            var sentWords = new string[]
+            {
+                "The Last of Us II",
+                "Days Gone",
+                "Far Cry IV",
+                "Miracle",
+                "Kafka",
+                "Server",
+                "Client",
+                "Udp",
+                "Tcp",
+                "Port",
+                "Ip"
+            };
+            await server.SendContinuousDatagrams(sentWords);
+        }
+    }
+}
