3e7e1c5c853abc42cc9e7495be77220612eced65
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-07-13 16:33:21 +0400

    UdpServer custom component is implemented

---
+++ Server/UdpServer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class UdpServer
+    {
+        public IPAddress IpAddress { get; }
+        public IPEndPoint LocalEndPoint { get; }
+        public UdpClient UdpSocket { get; }
+        public ConcurrentBag<IPEndPoint> ClientEndPoints { get; }
+
+        private readonly IPHostEntry _hostEntry = Dns.GetHostEntry("localhost");
+        private IPEndPoint _currentConnection;
+
+        public UdpServer(int port)
+        {
+            IpAddress = _hostEntry.AddressList[0];
+            LocalEndPoint = new IPEndPoint(IpAddress, port);
+            _currentConnection = new IPEndPoint(IPAddress.Any, 0);
+            UdpSocket = new UdpClient(LocalEndPoint);
+            ClientEndPoints = new ConcurrentBag<IPEndPoint>();
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var received = await UdpSocket.ReceiveAsync();
+                    var buffer = received.Buffer;
+                    Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, buffer.Length));
+                    ClientEndPoints.Add(received.RemoteEndPoint);
+                }
+            });
+        }
+
+        public async Task SendContinuousDatagrams(IEnumerable<string> words)
+        {
+            var datagrams = words.Select(w => Encoding.ASCII.GetBytes(w));
+            while (true)
+            {
+                await IterateDatagrams(datagrams);
+            }
+        }
+
+        private async Task IterateDatagrams(IEnumerable<byte[]> datagrams)
+        {
+            foreach (var dgram in datagrams)
+            {
+                await SendIterationElementToAllClients(dgram);
+            }
+        }
+
+        private async Task SendIterationElementToAllClients(byte[] dgram)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(600));
+            // foreach (var ep in ClientEndPoints)
+            // {
+            //     
+            // }
+
+            Parallel.ForEach(ClientEndPoints, async (ep) =>
+            {
+                await UdpSocket.SendAsync(dgram, dgram.Length, ep);
+            });
+
+
+        }
+    }
+}
